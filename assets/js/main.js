fff14c331f37d4ba9ea572aabe118ed0e93c1ec2
lorennaleon <68212806+lorennaleon@users.noreply.github.com>
2022-08-02 14:21:15 -0500

    Add files via upload

---
+++ assets/js/main.js
@@ -0,0 +1,74 @@
+/*==================== MENU SHOW Y HIDDEN ====================*/
+
+
+/*===== MENU SHOW =====*/
+
+const navMenu = document.getElementById('nav-menu'),
+    navToggle = document.getElementById('nav-toggle'),
+    navClose = document.getElementById('nav-close')
+
+
+/* Validate if constant exists */
+if(navToggle){
+    navToggle.addEventListener('click', ()=>{
+        navMenu.classList.add('show-menu')
+    })
+}
+/*===== MENU HIDDEN =====*/
+/* Validate if constant exists */
+if(navClose){
+    navClose.addEventListener('click',() => {
+       navMenu.classList.remove('show-menu')
+})
+}
+
+/*==================== REMOVE MENU MOBILE ====================*/
+const navLink = document.querySelectorAll('.nav_link')
+
+function linkAction(){
+    const navMenu = document.getElementById('nav-menu')
+    // When we click on each nav__link, we remove the show-menu class
+    navMenu.classList.remove('show-menu')
+}
+navLink.forEach(n => n.addEventListener('click', linkAction))
+
+/*==================== ACCORDION SKILLS ====================*/
+const skillsContent = document.getElementsByClassName('skills_content'),
+      skillsHeader = document.querySelectorAll('.skills_header')
+
+      function toggleSkills(){
+        let itemClass = this.parentNode.className
+
+        for(i=0; i < skillsContent.length; i++){
+            skillsContent[i].className = 'skills_content skills_close'
+        }
+        if(itemClass === 'skills_content skills_close'){
+            this.parentNode.className = 'skills_content skills_open'
+        }
+      }
+
+      skillsHeader.forEach((el) => {
+        el.addEventListener('click', toggleSkills)
+      })
+/*==================== QUALIFICATION TABS ====================*/
+
+
+/*==================== SERVICES MODAL ====================*/
+
+
+/*==================== PORTFOLIO SWIPER  ====================*/
+
+
+/*==================== TESTIMONIAL ====================*/
+
+
+/*==================== SCROLL SECTIONS ACTIVE LINK ====================*/
+
+
+/*==================== CHANGE BACKGROUND HEADER ====================*/ 
+
+
+/*==================== SHOW SCROLL UP ====================*/ 
+
+
+/*==================== DARK LIGHT THEME ====================*/ 
