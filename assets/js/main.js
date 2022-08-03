0a169f2ef18e7208574e8b212acdfb8f343fe519
bryan <xxbryan343@gmail.com>
2022-08-03 14:54:50 -0500

    servicios

--- assets/js/main.js
+++ assets/js/main.js
@@ -74,6 +74,27 @@ const skillsContent = document.getElementsByClassName('skills_content'),
 
 /*==================== SERVICES MODAL ====================*/
 
+  const modalViews = document.querySelectorAll('.services_modal'),
+        modalBtns = document.querySelectorAll('.services_button'),
+        modalCloses = document.querySelectorAll('.services_modal-close')
+
+    let modal= function(modalClick){
+        modalViews[modalClick].
+        classList.add('active-modal')
+    }
+
+    modalBtns.forEach((modalBtn, i) => {
+        modalBtn.addEventListener('click', () => {
+            modal(i)
+        })
+    })
+    modalCloses.forEach((modalClose) => {
+        modalClose.addEventListener('click', () => {
+            modalViews.forEach((modalView) => {
+               modalView.classList.remove('active-modal')
+            })
+        })
+    })
 
 /*==================== PORTFOLIO SWIPER  ====================*/
 
